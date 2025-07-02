5cb56403f1b5a5a4cce2693cdc541a15f6f2ea1c
Your name <you@example.com>
2025-07-02 16:20:36 +0800

    Show loading skeletons

---
+++ src/components/GameCardSkeleton.tsx
@@ -0,0 +1,15 @@
+import { Card, Skeleton, SkeletonText } from "@chakra-ui/react";
+import React from "react";
+
+const GameCardSkeleton = () => {
+  return (
+    <Card width="300px" borderRadius={10} overflow="hidden">
+      <Skeleton height="200px" />
+      <Card>
+        <SkeletonText />
+      </Card>
+    </Card>
+  );
+};
+
+export default GameCardSkeleton;
